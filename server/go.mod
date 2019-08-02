cd0aa6610d9a61a687622c166f86c1c95f72e2ca
Jordan Day <jordanday444@gmail.com>
2019-08-02 10:19:44 -0500

    Setup API

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/FryDay/housenumbers
 
 go 1.12
+
+require github.com/gorilla/mux v1.7.3
--- server/main.go
+++ server/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/color", getColor).Methods(http.MethodGet)
+	router.HandleFunc("/color", postColor).Methods(http.MethodPost)
+
+	router.HandleFunc("/time", getTime).Methods(http.MethodGet)
+	router.HandleFunc("/time", postTime).Methods(http.MethodPost)
+
+	log.Fatal(http.ListenAndServe(":8040", router))
+}
+
+func getColor(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	log.Println("GET /color", params)
+}
+
+func postColor(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	log.Println("POST /color", params)
+}
+
+func getTime(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	log.Println("GET /time", params)
+}
+
+func postTime(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	log.Println("POST /time", params)
 }
