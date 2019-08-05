c105f1b5e730f8ceae602673d729f1b89511b56b
Jordan Day <jordanday444@gmail.com>
2019-08-05 10:43:52 -0500

    Fix pointer issue

--- server/neopixel_arm.go
+++ server/neopixel_arm.go
@@ -9,11 +9,13 @@ import (
 )
 
 func init() {
+	var err error
+
 	opt := ws2811.DefaultOptions
 	opt.Channels[0].Brightness = brightness
 	opt.Channels[0].LedCount = ledCount
 
-	neopixel, err := ws2811.MakeWS2811(&opt)
+	neopixel, err = ws2811.MakeWS2811(&opt)
 	if err != nil {
 		panic(err)
 	}
